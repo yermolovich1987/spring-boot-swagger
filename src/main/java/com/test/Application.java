c7e86543a4ffe272a8371d16f8f0dd6ec0453ebb
Dmytro Iermolovych <Dmytro_Iermolovych@epam.com>
2017-01-31 00:03:03 +0200

    Implemented complete Greeting sample Spring Boot app

---
+++ src/main/java/com/test/Application.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Dmytro_Iermolovych on 1/30/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
